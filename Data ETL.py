8d397bb7c7f03a667813411fccd7ce4a895eeae0
Eli Kanowitz <elikanowitz27@gmail.com>
2023-06-14 14:46:36 -0400

    Updated

--- Data ETL.py
+++ Data ETL.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 # Read in the CSV file into a pandas dataframe
 df = pd.read_csv('combined_transactions.csv')
 
-# print(df)
+df['amount_in_dollars'] = df['amount_cents'] / 100
+df.drop(columns='amount_cents', inplace=True)
 
 # Split df into purchase and return and drop transaction_type column
-purchases_df = df[df['transaction_type'] == 'PurchaseActivity']
-returns_df = df[df['transaction_type'] == 'ReturnActivity']
-purchases_df.drop(columns='transaction_type', inplace=True)
-returns_df.drop(columns='transaction_type', inplace=True)
+purchases_df = df[df['transaction_type'] == 'PurchaseActivity'].copy()
+returns_df = df[df['transaction_type'] == 'ReturnActivity'].copy()
+# purchases_df.drop(columns='transaction_type', inplace=True)
+# returns_df.drop(columns='transaction_type', inplace=True)
 
 # print(purchases_df)
 # print(returns_df)
@@ -27,19 +28,50 @@ def create_db():
     returns_df.to_sql('returns', conn)
 
 
-# create_db()
+create_db()
+
 
 # Create cursor for SQL queries
-cursor = conn.cursor()
-purchase_rows = cursor.execute("SELECT * FROM purchases WHERE user_id=? UNION SELECT * FROM returns WHERE user_id=?")
-purchases = purchase_rows.fetchall()
+
+# purchase_rows = cursor.execute(
+#     "SELECT * FROM purchases WHERE user_id=333 UNION SELECT * FROM returns WHERE user_id=333")
+# global purchases
+# purchases = purchase_rows.fetchall()
+
+
+@app.route('/<int:num>', methods=['GET'])
+def get_transactions(num):
+    conn = sqlite3.connect('transactions.db')
+    print(num)
+    cursor = conn.cursor()
+    query = "SELECT * FROM purchases WHERE user_id=? "
+
+    cursor.execute(
+        "SELECT user_id, amount_in_dollars, datetime, merchant_type_code, transaction_type FROM purchases WHERE user_id=? UNION SELECT user_id, amount_in_dollars, datetime, merchant_type_code, transaction_type FROM returns WHERE user_id=?",
+        (num, num,))
+    purchases = cursor.fetchall()
+    conn.close()
+
+    transactions = []
+    for row in purchases:
+        transaction = {
+            'user_id': row[0],
+            'amount_in_dollars': row[1],
+            'datetime': row[2],
+            'merchant_type_code': row[3],
+            'transaction_type': row[4]
+        }
+        transactions.append(transaction)
+
+    response = {'transactions': transactions}
+    return jsonify(response)
+
 
 # print(purchases)
-cursor.close()
-conn.close()
-cols = [row[0] for row in purchase_rows.description]
-df1 = pd.DataFrame(purchases, columns=cols)
-print(df1)
+
+# cols = [row[0] for row in purchase_rows.description]
+# df1 = pd.DataFrame(purchases, columns=cols)
+# print(df1)
 
 if __name__ == '__main__':
     app.run()
