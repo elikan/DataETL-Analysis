8c214dcfaf8f1d893af2439f0765b9406581b287
Eli Kanowitz <elikanowitz27@gmail.com>
2023-06-14 17:21:40 -0400

    Updated

--- Data ETL.py
+++ Data ETL.py
@@ -1,77 +1,24 @@
 import pandas as pd
 import sqlite3
-from flask import Flask, jsonify
-
-app = Flask(__name__)
 
 # Read in the CSV file into a pandas dataframe
 df = pd.read_csv('combined_transactions.csv')
 
-df['amount_in_dollars'] = df['amount_cents'] / 100
+df['amount_in_dollars'] = round(df['amount_cents'] / 100, 2)
 df.drop(columns='amount_cents', inplace=True)
 
 # Split df into purchase and return and drop transaction_type column
 purchases_df = df[df['transaction_type'] == 'PurchaseActivity'].copy()
 returns_df = df[df['transaction_type'] == 'ReturnActivity'].copy()
-# purchases_df.drop(columns='transaction_type', inplace=True)
-# returns_df.drop(columns='transaction_type', inplace=True)
+purchases_df.drop(columns='transaction_type', inplace=True)
+returns_df.drop(columns='transaction_type', inplace=True)
 
-# print(purchases_df)
-# print(returns_df)
 # Connect to database
 conn = sqlite3.connect('transactions.db')
 
+# Write purchases and returns to respective tables
+purchases_df.to_sql('purchases', conn)
+returns_df.to_sql('returns', conn)
 
-def create_db():
-    # Write purchases and returns to respective tables
-    purchases_df.to_sql('purchases', conn)
-    returns_df.to_sql('returns', conn)
-
-
-create_db()
-
-
-# Create cursor for SQL queries
-
-# purchase_rows = cursor.execute(
-#     "SELECT * FROM purchases WHERE user_id=333 UNION SELECT * FROM returns WHERE user_id=333")
-# global purchases
-# purchases = purchase_rows.fetchall()
-
-
-@app.route('/<int:num>', methods=['GET'])
-def get_transactions(num):
-    conn = sqlite3.connect('transactions.db')
-    print(num)
-    cursor = conn.cursor()
-    query = "SELECT * FROM purchases WHERE user_id=? "
-
-    cursor.execute(
-        "SELECT user_id, amount_in_dollars, datetime, merchant_type_code, transaction_type FROM purchases WHERE user_id=? UNION SELECT user_id, amount_in_dollars, datetime, merchant_type_code, transaction_type FROM returns WHERE user_id=?",
-        (num, num,))
-    purchases = cursor.fetchall()
-    conn.close()
-
-    transactions = []
-    for row in purchases:
-        transaction = {
-            'user_id': row[0],
-            'amount_in_dollars': row[1],
-            'datetime': row[2],
-            'merchant_type_code': row[3],
-            'transaction_type': row[4]
-        }
-        transactions.append(transaction)
-
-    response = {'transactions': transactions}
-    return jsonify(response)
-
-
-# print(purchases)
-
-# cols = [row[0] for row in purchase_rows.description]
-# df1 = pd.DataFrame(purchases, columns=cols)
-# print(df1)
-
-if __name__ == '__main__':
-    app.run()
+# Close connection
+conn.close()
---
+++ webserver.py
@@ -0,0 +1,91 @@
+import sqlite3
+from flask import Flask, render_template, request, jsonify
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class Transactions(Resource):
+    def get(self, num):
+        # Connect to database
+        conn = sqlite3.connect('transactions.db')
+        # Create cursor for SQL queries
+        cursor = conn.cursor()
+        # SQL query to get user_id, amount_in_dollars, datetime, merchant_type_code, transaction_type for given user_id
+        cursor.execute('''
+        SELECT user_id, amount_in_dollars, datetime, merchant_type_code FROM purchases WHERE user_id=? 
+        UNION ALL
+        SELECT user_id, amount_in_dollars, datetime, merchant_type_code FROM returns WHERE user_id=?
+        ''', (num, num,))
+        # Fetch query results
+        rows = cursor.fetchall()
+
+        # Close connection
+        conn.close()
+
+        # For each row in query results out into dictionary to convert into JSON
+        transactions = []
+        for row in rows:
+            transaction = {
+                'user_id': row[0],
+                'amount_in_dollars': row[1],
+                'datetime': row[2],
+                'merchant_type_code': row[3],
+            }
+            transactions.append(transaction)
+
+        response = {'transactions': transactions}
+        # Convert to JSON
+        return jsonify(response)
+
+
+class Profit(Resource):
+    def get(self, num):
+        # Connect to database
+        conn = sqlite3.connect('transactions.db')
+        # Create cursor for SQL queries
+        cursor = conn.cursor()
+        # SQL query to get merchant_type_code and find profit for that merchant
+        cursor.execute('''
+        SELECT p.merchant_type_code, round((p.total_amount - COALESCE(r.total_amount, 0)),2) AS net_amount_in_dollars
+        FROM(
+        SELECT merchant_type_code, sum(amount_in_dollars) AS total_amount FROM purchases WHERE merchant_type_code=?) AS p
+        JOIN(
+        SELECT merchant_type_code, sum(amount_in_dollars) AS total_amount FROM returns WHERE merchant_type_code=?) AS r
+        ''', (num, num,))
+
+        # Fetch query results
+        rows = cursor.fetchall()
+
+        # SQL query to get datetime and conver to date for that merchant
+        cursor.execute('''
+              SELECT strftime('%Y-%m-%d', datetime) as date from purchases where merchant_type_code = ? 
+              UNION ALL SELECT strftime('%Y-%m-%d', datetime) as date from returns where merchant_type_code = ?
+              ''', (num, num,))
+        # Fetch query results
+        rows2 = cursor.fetchall()
+
+        # Close connection
+        conn.close()
+
+        # put merchant values in a dictionary to be converted into JSON
+        merchants = []
+        for row in rows:
+            merchant = {
+                'merchant_type_code': row[0],
+                'net_amount_in_dollars': row[1],
+                'date': rows2
+            }
+            merchants.append(merchant)
+        response = {'transactions': merchants}
+        # Convert to JSON
+        return jsonify(response)
+
+# Endpoint for all transactions of a user
+api.add_resource(Transactions, '/user/<int:num>')
+# Endpoint for totsl profet of a merchant
+api.add_resource(Profit, '/profit/<int:num>')
+
+if __name__ == '__main__':
+    app.run()
